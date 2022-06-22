911e6ab420d2e9ca99070b21783eaa35fbe1b893
ericmahare <erikmahare@gmail.com>
2022-06-22 18:39:55 +0300

    Refactor code to use fetched data

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,15 +1,19 @@
-const Country = () => {
+import { Link } from "react-router-dom";
+const Country = ({ info }) => {
+  const {administered, country, abbreviation} = info
   return (
-    <div className="country-card">
-     <img src='https://countryflagsapi.com/png/nigeria' alt='country-flag' className='country-icon'/>
-     <div className='card-text'>
-      <h2>Kenya</h2>
-      <h2>153,000</h2>
-     </div>
-     <div className='card-arrow'>
-        <p><i class="fa-solid fa-circle-arrow-right"></i></p>
-     </div>
-    </div>
+      <div className="country-card">
+    <Link to={`/${country}`}>
+      <img src={`https://countryflagsapi.com/png/${abbreviation}`} alt='country-flag' className='country-icon'/>
+      <div className='card-text'>
+        <h2>{country}</h2>
+        <h2>{administered.toLocaleString()}</h2>
+      </div>
+      <div className='card-arrow'>
+          <p><i className="fa-solid fa-circle-arrow-right"></i></p>
+      </div>
+    </Link>
+      </div>
   )
 }
 export default Country;
